b468b21d7ae9e7db9ee6034490c6971410ccd5f1
TinaNguyen <tinanguyen1783@gmail.com>
2022-06-21 00:56:50 -0400

    fix lab09

--- src/lab_09/AnimalsRaceResult.java
+++ src/lab_09/AnimalsRaceResult.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import static lab_09.Animal.Buider;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -51,28 +52,22 @@ public class AnimalsRaceResult {
 
     public static void main(String[] args) {
 
-        Animal Tiger = new Buider().setAnimalName("Tiger").setAnimalSpeed(100).setisWing(false).buildAnimal();
-        Animal Elephant = new Buider().setAnimalName("Elephant").setAnimalSpeed(50).setisWing(false).buildAnimal();
-        Animal Snake = new Buider().setAnimalName("Snake").setAnimalSpeed(10).setisWing(false).buildAnimal();
-        Animal Dog = new Buider().setAnimalName("Dog").setAnimalSpeed(30).setisWing(false).buildAnimal();
-        Animal Penguin = new Buider().setAnimalName("Penguin").setAnimalSpeed(15).setisWing(false).buildAnimal();
-        Animal Falcon = new Buider().setAnimalName("Falcon").setAnimalSpeed(120).setisWing(true).buildAnimal();
-        Animal Mosquito = new Buider().setAnimalName("Mosquito").setAnimalSpeed(30).setisWing(true).buildAnimal();
-        Animal Cat = new Buider().setAnimalName("Cat").setAnimalSpeed(25).setisWing(false).buildAnimal();
-        Animal Lion = new Buider().setAnimalName("Lion").setAnimalSpeed(100).setisWing(false).buildAnimal();
+        Buider buider = new Buider();
+
+        Animal Tiger = buider.setAnimalName("Tiger").setAnimalSpeed(100).setisWing(false).buildAnimal();
+        Animal Elephant = buider.setAnimalName("Elephant").setAnimalSpeed(50).setisWing(false).buildAnimal();
+        Animal Snake = buider.setAnimalName("Snake").setAnimalSpeed(10).setisWing(false).buildAnimal();
+        Animal Dog = buider.setAnimalName("Dog").setAnimalSpeed(30).setisWing(false).buildAnimal();
+        Animal Penguin = buider.setAnimalName("Penguin").setAnimalSpeed(15).setisWing(false).buildAnimal();
+        Animal Falcon = buider.setAnimalName("Falcon").setAnimalSpeed(120).setisWing(true).buildAnimal();
+        Animal Mosquito = buider.setAnimalName("Mosquito").setAnimalSpeed(30).setisWing(true).buildAnimal();
+        Animal Cat = buider.setAnimalName("Cat").setAnimalSpeed(25).setisWing(false).buildAnimal();
+        Animal Lion = buider.setAnimalName("Lion").setAnimalSpeed(100).setisWing(false).buildAnimal();
 
         List<Animal> animalList = new ArrayList<>();
         List<Animal> animalWinList = new ArrayList<>();
 
-        animalList.add(Tiger);
-        animalList.add(Elephant);
-        animalList.add(Snake);
-        animalList.add(Dog);
-        animalList.add(Penguin);
-        animalList.add(Falcon);
-        animalList.add(Mosquito);
-        animalList.add(Cat);
-        animalList.add(Lion);
+        animalList = Arrays.asList(Tiger, Elephant, Snake, Dog, Penguin, Falcon, Mosquito, Cat, Lion);
 
         animalWinList = new AnimalsRaceResult().getAnimalWinList(animalList);
         String nameOfAnimalWinList = "";
