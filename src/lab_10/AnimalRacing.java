ecbf2d9501cf4b7b7374144b545bcc4dd3d23426
TinaNguyen <tinanguyen1783@gmail.com>
2022-06-23 13:10:05 -0400

    lab 10

--- src/lab_10/AnimalRacing.java
+++ src/lab_10/AnimalRacing.java
@@ -1,26 +1,20 @@
 package lab_10;
 
-import lab_08_2.Animal;
-import lab_08_2.Dog;
-import lab_08_2.Horse;
-import lab_08_2.Tiger;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 
 public class AnimalRacing {
 
-    public void getAnimalWin(List<lab_08_2.Animal> animalList) {
+    public void getAnimalWin(List<Animal> animalList) {
 
-        ArrayList<lab_08_2.Animal> animalWinList = new ArrayList<>();
+        ArrayList<Animal> animalWinList = new ArrayList<>();
         animalWinList.add(animalList.get(0));
 
         int maxSpeed;
         maxSpeed = animalList.get(0).getSpeed();
 
-        HashMap<Integer, ArrayList<lab_08_2.Animal>> speedRecord = new HashMap<>();
-        speedRecord.put(maxSpeed, animalWinList);
 
         for (int i = 1; i < animalList.size(); i++) {
 
@@ -28,13 +22,10 @@ public class AnimalRacing {
 
             if (maxSpeed < animalList.get(i).getSpeed()) {
 
-                speedRecord.remove(maxSpeed);
                 animalWinList.clear();
 
                 maxSpeed = animalList.get(i).getSpeed();
                 animalWinList.add(animalList.get(i));
-
-                speedRecord.put(maxSpeed, animalWinList);
 
             }
         }
@@ -52,23 +43,16 @@ public class AnimalRacing {
 
     }
 
-    public static List<lab_08_2.Animal> setAnimalList() {
+    public static List<Animal> setAnimalList() {
 
         String nameAnimal;
-        lab_08_2.Animal dog = new Dog();
-        lab_08_2.Animal horse = new Horse();
-        lab_08_2.Animal tiger = new Tiger();
+        Animal dog = new Dog();
+        Animal horse = new Horse();
+        Animal tiger = new Tiger();
 
-//        dog.setSpeed(9);
-//        horse.setSpeed(30);
-//        tiger.setSpeed(30);
+        List<Animal> animalList = new ArrayList<>();
+        animalList=Arrays.asList(dog,horse,tiger);
 
-        List<lab_08_2.Animal> animalList = new ArrayList<>();
-        List<lab_08_2.Animal> animalWinList = new ArrayList<>();
-
-        animalList.add(dog);
-        animalList.add(horse);
-        animalList.add(tiger);
         for (Animal animal : animalList) {
 
             System.out.printf("%s speed: %d\n", animal.getClass().getSimpleName(), animal.getSpeed());
